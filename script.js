01991e3df9a61fc0c907fe4d38d66b2483d40f2b
LeeRuss <96124920+LeeRuss@users.noreply.github.com>
2022-06-17 15:44:26 +0200

    calculator script
    whole calculator logic done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var firstNumber;
-var secondNumber;
-var currentNumber=1;
+var currentNumber = "";
+var lastNumber = "";
+var currentOperation = "";
 var numberButtons = document.getElementsByClassName("number-button");
 var operationButtons = document.getElementsByClassName("operation-button");
 var equalsButton = document.getElementById("equals-button");
@@ -11,22 +11,69 @@ initCalc();
 function initCalc(){
     for(var i=0; i<numberButtons.length; ++i){
         numberButtons[i].addEventListener("click", function(){
-            
+            var containDot = (currentNumber.search(/\./) != -1);
+            console.log(currentNumber.search(/\./));
+            console.log(containDot);
+            if(this.value != "C"){
+                if(this.value == "."){
+                    if(!containDot && currentNumber.length < 17){
+                        currentNumber = currentNumber+this.value;
+                    }
+                }else if(containDot && currentNumber.length < 17){
+                    currentNumber = currentNumber+this.value;
+                    }else if(currentNumber.length < 16){
+                        currentNumber = currentNumber+this.value;
+                    }
+            }else{
+                currentNumber = "";
+            } 
+            printOnDisplay(currentNumber);
         })
     }
 
     for(var i=0; i<operationButtons.length; ++i){
         operationButtons[i].addEventListener("click", function(){
-            console.log(this.value);
+            if(currentOperation != ""){
+                equals();
+            }
+            currentOperation = this.value;
+            lastNumber = currentNumber;
+            currentNumber = "";
         })
     }
     
     equalsButton.addEventListener("click", function(){
-        console.log(this.value);
+        equals();
+        currentNumber = "";
     })
 
 }
 
-function swapCurrentNumber(){
-    if()
+function printOnDisplay(number){
+    calculatorDisplay.textContent = number;
 }
+
+function equals(){
+    currentNumber = parseFloat(currentNumber);
+    lastNumber = parseFloat(lastNumber);
+    switch(currentOperation) {
+        case "+":
+          currentNumber = lastNumber + currentNumber;
+          break;
+        case "-":
+            currentNumber = lastNumber - currentNumber;
+          break;
+        case "*":
+            currentNumber = lastNumber * currentNumber;
+          break;
+        case "/":
+            currentNumber = lastNumber / currentNumber;
+            break;
+        default: 
+            break;
+      }
+      currentOperation = "";
+      lastNumber = currentNumber;
+      printOnDisplay(currentNumber);
+}
+
